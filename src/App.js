8c5d3a7ffa206a93c55b93e95621f4a17238a9ca
Derek Cooper <derekcooper20@gmail.com>
2022-10-17 16:20:58 -0600

    Adds funcitons and jsx for iterations 1-3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
 import MOVIES from './MoviesData';
 import Movies from './Movies';
-import React from 'react';
+import CardDetails from './CardDetails';
+import React, { useState } from 'react';
 import './App.css';
 
 
 class App extends React.Component {
   constructor() {
     super()
-    this.state = { movies: MOVIES }
+    this.state = { movies: [], selectedMovie: null, visible: true }
   }
+
+
+  componentDidMount = () => {
+    this.setState({ visible: true })
+    fetch('https://rancid-tomatillos.herokuapp.com/api/v2/movies')
+      .then(response => response.json())
+      .then(data => { this.setState({ movies: data.movies }) })
+  }
+
+  handleChange = (event) => {
+
+    const findMovie = this.state.movies.find(movie => movie.id === parseInt(event.target.id))
+
+    this.setState({ selectedMovie: findMovie, visible: !this.state.visible })
+  }
+
+
 
   render() {
     const getMovies = this.state.movies.map((movie) => {
-      return <Movies key={movie.movie.id} movie={movie.movie} />
+      // console.log(movie)
+      return <Movies key={movie.id} movie={movie} handleChange={this.handleChange} />
+
     })
     return (
       <div>
         <h1 className="site-title">Rancid Tomatillos</h1>
-        {getMovies} {getMovies}
+        {this.state.visible ? <div className='all-movies-container'>{getMovies}</div> : <CardDetails movie={this.state.selectedMovie} handleChange={this.handleChange} />}
       </div>
     )
   }
--- src/Movies.js
+++ src/Movies.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import CardDetails from './CardDetails';
 import './Movies.css'
 
 class Movies extends React.Component {
   render() {
     return (
-        <section className="movie-container"> 
-      <div>
-        <img className="movie-poster" src={this.props.movie.poster_path} alt="movie poster" />
-        <p>{this.props.movie.title} <br/>
-          {`Release Date ${this.props.movie.release_date} ~  ${this.props.movie.runtime} min `}
-        </p>
-      </div>
+      <section className="movie-container">
+        <div>
+          <img onClick={(event) => this.props.handleChange(event)} id={this.props.movie.id}  className="movie-poster" src={this.props.movie.poster_path} alt="movie poster" />
+          <p>{this.props.movie.title} <br />
+            {`Release Date ${this.props.movie.release_date} ~  ${this.props.movie.average_rating} rating `}
+          </p>
+        </div>
       </section>
     );
   }
